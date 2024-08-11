e67cfefe8b32ea13149d98547915c1d9d509eb50
homer <homer@simpson.simpson>
2024-08-11 14:41:33 -0400

    Dang Fleas

--- Scripts/flea.gd
+++ Scripts/flea.gd
@@ -1,23 +1,36 @@
 extends CharacterBody2D
 
 @onready var flea = $AnimatedSprite2D
-@onready var hidebox = $HideBox
-var player_in_box : bool = false
+@export var speed : float = 50
+@export var moving_left : bool = true
+@export var starting_position : Vector2
+@export var move_spaces : int = 200
 
 
 func _ready():
+	starting_position = position
 	flea.flip_h = true
 	flea.play("walk")
 
 
-func _process(delta):
-	if $EnemyMovementComponent.moving_left == true:
-		flea.flip_h = true
-	elif $EnemyMovementComponent.moving_left == false:
-		flea.flip_h = false
-	
 func _physics_process(delta):
+	check_wall()
+	if moving_left == true:
+		flea.flip_h = true
+		position.x -= speed * delta
+		if position.x <= starting_position.x - move_spaces:
+			moving_left = false
+	elif moving_left == false:
+		flea.flip_h = false
+		position.x += speed * delta
+		if position.x >= starting_position.x:
+			moving_left = true
 	move_and_slide()
 
-func _on_comp_player_hide_signal_hide_player(message):
-	player_in_box = true
+func check_wall():
+	if is_on_wall():
+		if moving_left == true:
+			moving_left = false
+		else:
+			moving_left = true
+	
