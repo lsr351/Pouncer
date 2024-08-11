5fcdeb7f7408bd605e6aadb906d4b65d233c3b99
lsr351 <>
2024-08-11 20:03:58 +0300

    Add coyote time to player character
    Move player logic to _physics_process. Physics stuff should be there.
    Also changed y velocity to be applied exactly to jump strength.

--- Scripts/PouncerTwo.gd
+++ Scripts/PouncerTwo.gd
@@ -5,8 +5,11 @@ extends CharacterBody2D
 @onready var pouncer = $AnimatedSprite2D
 @export var jump_strength = 100
 @export var speed = 175
+@onready var coyote_timer: Timer = $CoyoteTimer
 
-func _process(delta):
+var was_on_floor := false
+
+func _physics_process(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	if input_direction: # Plays the run animation and idle
 		pouncer.play("run")
@@ -18,16 +21,19 @@ func _process(delta):
 		pouncer.flip_h = false
 	elif input_direction == Vector2(-1, 0):
 		pouncer.flip_h = true
-
-	if is_on_floor():
-		if Input.is_action_pressed("jump"):
-			velocity.y -= jump_strength * speed * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or not coyote_timer.is_stopped():
+			velocity.y = -jump_strength * speed * delta
 	if !is_on_floor():
 		pouncer.play("jump")
-
+	
 	velocity.x = input_direction.x * speed
-func _physics_process(delta):
+	was_on_floor = is_on_floor()
 	move_and_slide()
+	
+	if not is_on_floor() and was_on_floor:
+		coyote_timer.start()
 
 
 # Currently the flee kills us
